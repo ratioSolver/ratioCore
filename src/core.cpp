c389933d9922926c47196fc488eeb5b8e009e307
Riccardo De Benedictis <riccardo.debenedictis@istc.cnr.it>
2022-07-19 08:15:00 +0200

    solved minor compatibility issue..

--- src/core.cpp
+++ src/core.cpp
@@ -23,33 +23,33 @@ namespace ratio::core
     }
 
     ORATIOCORE_EXPORT expr new_bool() noexcept { return nullptr; }
-    ORATIOCORE_EXPORT expr new_bool(const bool &val) noexcept { return nullptr; }
+    ORATIOCORE_EXPORT expr new_bool(const bool &) noexcept { return nullptr; }
     ORATIOCORE_EXPORT expr new_int() noexcept { return nullptr; }
-    ORATIOCORE_EXPORT expr new_int(const semitone::I &val) noexcept { return nullptr; }
+    ORATIOCORE_EXPORT expr new_int(const semitone::I &) noexcept { return nullptr; }
     ORATIOCORE_EXPORT expr new_real() noexcept { return nullptr; }
-    ORATIOCORE_EXPORT expr new_real(const semitone::rational &val) noexcept { return nullptr; }
+    ORATIOCORE_EXPORT expr new_real(const semitone::rational &) noexcept { return nullptr; }
     ORATIOCORE_EXPORT expr new_time_point() noexcept { return nullptr; }
-    ORATIOCORE_EXPORT expr new_time_point(const semitone::rational &val) noexcept { return nullptr; }
+    ORATIOCORE_EXPORT expr new_time_point(const semitone::rational &) noexcept { return nullptr; }
     ORATIOCORE_EXPORT expr new_string() noexcept { return nullptr; }
-    ORATIOCORE_EXPORT expr new_string(const std::string &val) noexcept { return nullptr; }
-    ORATIOCORE_EXPORT expr new_enum(type &tp, std::vector<expr> allowed_vals) { return nullptr; }
+    ORATIOCORE_EXPORT expr new_string(const std::string &) noexcept { return nullptr; }
+    ORATIOCORE_EXPORT expr new_enum(type &, std::vector<expr>) { return nullptr; }
 
-    ORATIOCORE_EXPORT expr negate(expr var) noexcept { return nullptr; }
-    ORATIOCORE_EXPORT expr conj(const std::vector<expr> &exprs) noexcept { return nullptr; }
-    ORATIOCORE_EXPORT expr disj(const std::vector<expr> &exprs) noexcept { return nullptr; }
-    ORATIOCORE_EXPORT expr exct_one(const std::vector<expr> &exprs) noexcept { return nullptr; }
+    ORATIOCORE_EXPORT expr negate(expr) noexcept { return nullptr; }
+    ORATIOCORE_EXPORT expr conj(const std::vector<expr> &) noexcept { return nullptr; }
+    ORATIOCORE_EXPORT expr disj(const std::vector<expr> &) noexcept { return nullptr; }
+    ORATIOCORE_EXPORT expr exct_one(const std::vector<expr> &) noexcept { return nullptr; }
 
-    ORATIOCORE_EXPORT expr add(const std::vector<expr> &exprs) noexcept { return nullptr; }
-    ORATIOCORE_EXPORT expr sub(const std::vector<expr> &exprs) noexcept { return nullptr; }
-    ORATIOCORE_EXPORT expr mult(const std::vector<expr> &exprs) noexcept { return nullptr; }
-    ORATIOCORE_EXPORT expr div(const std::vector<expr> &exprs) noexcept { return nullptr; }
-    ORATIOCORE_EXPORT expr minus(expr ex) noexcept { return nullptr; }
+    ORATIOCORE_EXPORT expr add(const std::vector<expr> &) noexcept { return nullptr; }
+    ORATIOCORE_EXPORT expr sub(const std::vector<expr> &) noexcept { return nullptr; }
+    ORATIOCORE_EXPORT expr mult(const std::vector<expr> &) noexcept { return nullptr; }
+    ORATIOCORE_EXPORT expr div(const std::vector<expr> &) noexcept { return nullptr; }
+    ORATIOCORE_EXPORT expr minus(expr) noexcept { return nullptr; }
 
-    ORATIOCORE_EXPORT expr lt(expr left, expr right) noexcept { return nullptr; }
-    ORATIOCORE_EXPORT expr leq(expr left, expr right) noexcept { return nullptr; }
-    ORATIOCORE_EXPORT expr eq(expr left, expr right) noexcept { return nullptr; }
-    ORATIOCORE_EXPORT expr geq(expr left, expr right) noexcept { return nullptr; }
-    ORATIOCORE_EXPORT expr gt(expr left, expr right) noexcept { return nullptr; }
+    ORATIOCORE_EXPORT expr lt(expr, expr) noexcept { return nullptr; }
+    ORATIOCORE_EXPORT expr leq(expr, expr) noexcept { return nullptr; }
+    ORATIOCORE_EXPORT expr eq(expr, expr) noexcept { return nullptr; }
+    ORATIOCORE_EXPORT expr geq(expr, expr) noexcept { return nullptr; }
+    ORATIOCORE_EXPORT expr gt(expr, expr) noexcept { return nullptr; }
 
     ORATIOCORE_EXPORT expr core::get(const std::string &name) noexcept
     {
